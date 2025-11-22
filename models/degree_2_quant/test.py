48bd1cc0daea5419880381df0d41d71331c910cb
VimalWill <vimalwilliam99@gmail.com>
2025-11-22 09:21:12 +0530

    improved mac and quant

--- models/degree_2_quant/test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-path = "attn_float_activations.npz"  # 或 quant 的那份
-data = np.load(path)
-print("共有", len(data.files), "个数组：")
-for k in data.files:
-    print(f"{k:60s} shape={data[k].shape}, dtype={data[k].dtype}")
